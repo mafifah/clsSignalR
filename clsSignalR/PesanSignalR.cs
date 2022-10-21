c7f86b165bd6252f6007432ad6b2e9e811fb97f3
M Afifah <mafifah.usr@gmail.com>
2022-10-21 14:48:13 +0700

    Add project files.

---
+++ clsSignalR/PesanSignalR.cs
@@ -0,0 +1,21 @@
+﻿namespace clsSignalR
+{
+    public class PesanSignalR
+    {
+        public string Klien { get; set; }
+        public string Divisi { get; set; }
+        public object IdUser { get; set; }
+        public Guid IdSignal { get; set; }
+        public long IdForm { get; set; }
+        public string NamaForm { get; set; }
+        public string StatusAction { get; set; }
+        public string NamaFieldPK { get; set; }
+        public object NilaiPK { get; set; }
+        public DateTimeOffset WaktuProses { get; set; }
+        public string IsiPesan { get; set; }
+        public string JenisPesan { get; set; }
+
+        public string NamaFolderThumbnail { get; set; }
+        public string NamaFileThumbnail { get; set; }
+    }
+}
---
+++ clsSignalR/pthSignalRService.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace clsSignalR
+{
+    public class pthSignalRService
+    {
+        private HubConnection _hc { get; set; }
+        private string _divisi { get; set; }
+        private string _klien { get; set; }
+
+        public pthSignalRService()
+        {
+            HttpClientHandler HttpCh = new HttpClientHandler();
+            HttpCh.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            HttpClient client = new HttpClient(HttpCh);
+            client.BaseAddress = new Uri("http://assignalrserver.azurewebsites.net");
+            _hc = new HubConnectionBuilder().WithUrl(client.BaseAddress).Build();
+            _hc.On<Action<PesanSignalR>, bool>("TerimaPesan", TerimaPesan);
+        }
+
+        public async Task MulaiKoneksi(string klien)
+        {
+            _klien = klien;
+            if (_hc.ConnectionId != null)
+            {
+                await Disconnect();
+            }
+            await _hc.StartAsync();
+            await _hc.InvokeAsync("MulaiKoneksi", _klien);
+        }
+
+        public async Task Disconnect()
+        {
+            try
+            {
+                await _hc.InvokeAsync("StopKoneksi", _klien);
+                await _hc.StopAsync();
+            }
+            catch (Exception e)
+            {
+                var msg = e.Message;
+            }
+        }
+
+        public async Task KirimPesan(long IdForm, string NamaForm, string StatusAction, string NamaFieldPK, object NilaiPK, DateTimeOffset WaktuProses, string IsiPesan, string platform)
+        {
+            var msg = $"{platform}_{_klien}";
+            var psr = new PesanSignalR
+            {
+                Klien = _klien,
+                Divisi = "",
+                IdUser = "",
+                IdSignal = Guid.NewGuid(),
+                IdForm = IdForm,
+                NamaForm = NamaForm,
+                StatusAction = StatusAction,
+                NamaFieldPK = NamaFieldPK,
+                NilaiPK = NilaiPK,
+                WaktuProses = WaktuProses,
+                IsiPesan = IsiPesan,
+                JenisPesan = msg
+            };
+            try
+            {
+                if (_hc.State == HubConnectionState.Connected)
+                {
+                    await _hc.InvokeAsync("KirimPesan", psr);
+                }
+            }
+            catch (Exception ex) {
+                var x = ex.Message;
+            }
+        }
+
+        public void TerimaPesan(Action<PesanSignalR> a_psr = null, bool isBroadcast = false)
+        {
+            if (isBroadcast)
+            {
+                _hc.On("KirimPesanBroadcast", a_psr);
+            }
+            else
+            {
+                _hc.On("KirimPesan", a_psr);
+            }
+        }
+    }
+}
